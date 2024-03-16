77bef72680fb60e25427299a370fe3ab5969d760
Dercio <12derciomaduna@gmail.com>
2024-03-16 01:38:49 -0700

    added new files again fixing 2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
+import sys
+
+def main():
+    # Get the number of arguments (excluding the script name)
+    num_args = len(sys.argv) - 1
+
+    # Print the number of arguments
+    print(f"{num_args} argument{'s' if num_args != 1 else ''}:", end=' ')
+
+    # Print each argument along with its position
+    for i, arg in enumerate(sys.argv[1:], start=1):
+        print(f"\n{i}: {arg}")
 
 if __name__ == "__main__":
-    """Print the number of and list of arguments."""
-    import sys
-
-    count = len(sys.argv) - 1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
-        
+    main()
